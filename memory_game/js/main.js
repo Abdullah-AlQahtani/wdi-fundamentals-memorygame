14f9f4265780233fbb66a99906f23a2601d9bfad
BuildTools <unconfigured@null.spigotmc.org>
2018-09-26 02:46:49 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "King";
-var cardThree = "queen";
-var cardFour = "King";
+var cards = ['queen','queen','King','King'];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+}
+    else {
+        alert("Sorry, try again.");
+
+    }
